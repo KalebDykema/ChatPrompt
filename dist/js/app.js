9460388769f72bda1c83c3c9a8100bf2fe2fcdd6
Kaleb Dykema <dykemakaleb@gmail.com>
2021-03-30 23:46:12 -0500

    Fixed issues related to typing messages not going away after sending a message

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,57 +1,79 @@
+// Socket Object
 let socket = io()
 
+// Local Name Variable
 let name = 'N/A'
 
+// DOM Objects
 let nameForm = document.getElementById('enter-name')
 let nameInput = document.getElementById('input-name')
 let messages = document.getElementById('chat-history')
 let messageForm = document.getElementById('send-message')
 let messageInput = document.getElementById('input-message')
 
+// Key Down Listener
 document.onkeydown = function(e){
+  // Looks if Enter is clicked and then saves a chosen name to a local variable
   if(e.key == 'Enter' && nameInput.value.trim() != ''){
     e.preventDefault()
     name = nameInput.value.trim()
     nameInput.value = ''
 
+    // Gets rid of the name form and displays the messages and message form
     nameForm.style.display = 'none'
     messages.style.display = 'block'
     messageForm.style.display = 'flex'
+    // Looks if Enter is clicked and then emits a message to the server
   } else if(e.key == 'Enter' && messageInput.value.trim() != ''){
     e.preventDefault()
     socket.emit('chat message', name, messageInput.value.trim())
     messageInput.value = '';
   }
 
+  // Looks to see if the user is typing and emits that to the server
   if(nameForm.style.display == 'none' && messageInput.value != ''){ 
     socket.emit('typing', name, messageInput.value)
   }
 }
 
+// Key Up Listener
 document.onkeyup = function(){
+  // Looks to see if the user is typing and emits that to the server
   if(nameForm.style.display == 'none' && messageInput.value != ''){ 
     socket.emit('typing', name, messageInput.value)
   }
 }
 
+// On Chat Message Socket Received
 socket.on('chat message', function(name, msg) {
+  // If there's a typing message for the user, removes it
   if(messages.querySelectorAll('.typing')){
     messages.querySelector(`.${name}`).remove()
   }
+  // Create the message element and display it to the dom
   let item = document.createElement('pre')
   item.classList = "message"
   item.textContent = '> ' + name + ': ' + msg
   messages.appendChild(item)
+  // If there's any typing messages from other users, push them under the new message
+  if(messages.querySelectorAll('.typing')){
+    messages.querySelectorAll('.typing').forEach((typingMsg) => {
+      messages.appendChild(typingMsg)
+    })
+  }
   messages.scrollTo(0, messages.scrollHeight)
 })
 
+// On Typing Socket Received
 socket.on('typing', function(name, msg) {
+  // If the user doesn't already have a typing message, creates one and displays it in the DOM
   if(!messages.querySelector(`.typing.${name}`) && msg != ''){
     let item = document.createElement('p')
     item.classList = `typing ${name}`
     item.textContent = `${name} is typing`
     messages.appendChild(item)
     messages.scrollTo(0, messages.scrollHeight)
+    // If the user's message field is now empty, removes their typing message form the DOM 
   } else if(messages.querySelector(`.typing.${name}`) && msg == '') {
     messages.querySelector(`.typing.${name}`).remove()
   }
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+// Server Objects
 var express = require('express')
 var app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
+// Sends the CSS and JS files to the client
 app.use('/css', express.static(__dirname + '/dist/css'));
 app.use('/js', express.static(__dirname + '/dist/js'));
 
+// Send the HTML index to the client
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/dist/index.html')
 })
 
+// Listen for different sockets after the user connects
 io.on('connection', (socket) => {
   console.log('a user connected')
   socket.on('disconnect', () => {
@@ -23,6 +27,7 @@ io.on('connection', (socket) => {
   })
 })
 
+// Tell the server to run on port 3000
 http.listen(3000, () => {
   console.log('listening on *:3000')
 })
